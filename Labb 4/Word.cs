097c8146e24dcec95014f6793591cd7afe98955b
Magnus <Magnus.Johansson@iths.se>
2020-10-12 10:36:18 +0200

    Initial commit

---
+++ Labb 4/Word.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLibrary
+{
+    public class Word
+    {
+        public string[] Translations { get; }
+        public int FromLanguage { get; }
+        public int ToLanguage { get; }
+        public Word(params string[] translations)
+        {
+            //Konstruktor
+            //initialiserar ’Translations’ med data som skickas in som ’translations’ 
+        }
+        public Word(int fromLanguage, int toLanguage, params string[] translations)
+        {
+            //Konstruktor2
+            //som ovan, fast sätter även FromLanguage och ToLanguage.
+        }
+    }
+}
